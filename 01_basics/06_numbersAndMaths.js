5f435a78c9b4c45296c3cddebb080d36caa57857
surishetty vamshidhar <130429285+vamshisurishetty@users.noreply.github.com>
2025-12-10 18:21:23 +0000

    done

---
+++ 01_basics/06_numbersAndMaths.js
@@ -0,0 +1,17 @@
+const score = 100
+
+const balance = new Number(999)
+// console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));  // get 2 zeros after (.) point
+
+const otherNumber = 23.45678
+// console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(2));
+// console.log(otherNumber.toPrecision(4));
+
+const otherNumber1 = 123.45678
+// console.log(otherNumber1.toPrecision(3));
+
