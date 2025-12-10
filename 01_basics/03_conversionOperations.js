1e81567303417a6af4481f019515146a65033f65
surishetty vamshidhar <130429285+vamshisurishetty@users.noreply.github.com>
2025-12-10 07:44:51 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,42 @@
+// let score = 77 // number
+// let score = "77abc"  // string
+// let score = null // output => 0
+// let score = undefined // NaN
+// let score = true // 1
+// let score = false // 0
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN , but 33abc type is number
+// true => 1 , false => 0
+
+
+// let isLoggedIn = 1 // true
+// let isLoggedIn = "" // false
+// let isLoggedIn = "vamshi" // true
+
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// "" => false
+// "vamshi" => true
+
+
+
+let noNumber = 33
+
+let stringNumber = String(noNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
