b523489054fff602a93505d8b42bacb8c690c42d
surishetty vamshidhar <130429285+vamshisurishetty@users.noreply.github.com>
2025-12-10 11:57:12 +0000

    conversion and operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -36,7 +36,47 @@
 let noNumber = 33
 
 let stringNumber = String(noNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ******************************** Operations *********************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+let str1 = "hello"
+let str2 = " vamshi"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);  // => 112
+// console.log(1 + 2 + "2");  // => 32 
+
+// console.log( (3 + 4) * 5 % 3);
+
+
+// console.log(true);
+// console.log(+true);  // => 1
+// console.log("");
+// console.log(+"");  // => 0
+
+
+let counter = 100
+counter++
+console.log(counter);
+
+// learn prefix and postfix in mdn javascript
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
 
 
