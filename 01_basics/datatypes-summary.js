d9f308baa2cb5cfb6348ae14bde715daabd2fe57
surishetty vamshidhar <130429285+vamshisurishetty@users.noreply.github.com>
2025-12-10 12:44:21 +0000

    stack and heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -34,9 +34,9 @@ const myFunction = function(){
     console.log("hello world");
 }
 
-console.log(typeof bigNumber); // bigInt
-console.log(typeof myFunction);
-console.log(typeof anotherid);
+// console.log(typeof bigNumber); // bigInt
+// console.log(typeof myFunction);
+// console.log(typeof anotherid);
 
 
 
@@ -51,3 +51,54 @@ console.log(typeof anotherid);
 
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// memories : stack and heap
+
+// primitive data types stored in stack 
+// non-primitive data types stored in heap memory
+
+// stack(primitive)  and heap (non-primitive)
+
+
+// Primitive = copy. Changing the copy does not change the original.
+
+// Non-primitive = reference. Changing it does change the original because they share the same “box.”
+
+// Primitive
+let a = 5;
+let b = a;   // b gets a copy
+b = 10;      // change b
+// a is still 5
+
+// Non-primitive
+let obj1 = { toy: "car" };
+let obj2 = obj1;   // same box (reference)
+obj2.toy = "ball"; // change one
+// obj1.toy is now "ball"
+
+
+let myYtname = "vamshiSurishetty"
+
+let newYtname = myYtname
+newYtname = "surishettyVamshidhar"
+
+console.log(newYtname);
+// Primitive types = only copy, no shared reference.
+
+
+
+let user1 = {
+    email: "user@google.com",
+    upi: "user@ybl",
+}
+
+let user2 = user1
+
+user2.email = "user@youTube.com"
+
+console.log(user1);
+console.log(user2);
