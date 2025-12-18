e2daeaef7d92ddd8495f0d519b4cddeecf2bda24
surishetty vamshidhar <130429285+vamshisurishetty@users.noreply.github.com>
2025-12-18 09:16:32 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,57 @@
+//switch syntax
+
+// switch (key){
+//     case value:
+//         break:
+//     default:
+//         break;
+// }
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+//     case 5:
+//         console.log("may");
+//         break;
+
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+const Month = "march"
+
+switch (Month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    case "may":
+        console.log("may");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
