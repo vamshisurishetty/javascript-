847a2210e88d338e19effbca0645fc2ceafef978
surishetty vamshidhar <130429285+vamshisurishetty@users.noreply.github.com>
2025-12-21 18:04:05 +0000

    for loop (break and continue)

---
+++ 05_iterations/one.js
@@ -0,0 +1,69 @@
+// for loop
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element); 
+// }
+
+
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element); 
+// }
+
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if(element == 5){
+        // console.log("5 is best number");
+    }
+    // console.log(element); 
+}
+
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`outer loop value: ${i}`);
+//     for(let j = 0; j <= 10; j++){
+//         console.log(`inner loop value: ${j} and inner loop ${i}`);
+//     }
+// }
+
+
+for (let i = 0; i <= 10; i++) {
+    // console.log(`outer loop value: ${i}`);
+    for(let j = 0; j <= 10; j++){
+        // console.log(i + '*' + j + ' = ' + i*j);
+    }
+}
+
+
+
+let myArray = ["luffy", "garp", "dargon"]
+// console.log(myArray.length);
+
+for(let index = 0; index < myArray.length; index++){
+    const element = myArray[index];
+    // console.log(element);
+}
+
+
+// imp things in loops
+
+// break and continue
+
+// for( let i = 1; i <= 20; i++){
+//     if(i == 5){
+//         console.log(`this is the num i want: ${i}`);
+//         break
+//     }
+//     console.log(`value of i is: ${i}`);
+// }
+
+
+for( let i = 1; i <= 20; i++){
+    if(i == 5){
+        console.log(`this is the num i want: ${i}`);
+        continue
+    }
+    console.log(`value of i is: ${i}`);
+}
