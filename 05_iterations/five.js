017374050856060267d1db46ff27c3f85d98d632
surishetty vamshidhar <130429285+vamshisurishetty@users.noreply.github.com>
2025-12-22 07:56:58 +0000

    forEach using function expression and arrow functions and on objects also

---
+++ 05_iterations/five.js
@@ -0,0 +1,42 @@
+//commonly used loops
+
+
+const coding = ["js", "ruby", "java", "cpp", "python"]
+
+
+//using function expression
+// coding.forEach( function (item) {
+//     console.log(item);
+// })
+
+
+//using arrow function
+// coding.forEach( (item) => {
+//         console.log(item);    
+// })
+
+
+
+// coding.forEach( (item, index, arr) => {
+//         console.log(item, index, arr);
+// })
+
+
+const myCoding = [
+        {
+                languageName: "javascript",
+                languageFileName: "js"
+        },
+        {
+                languageName: "java",
+                languageFileName: "java"
+        },
+        {
+                languageName: "python",
+                languageFileName: "py"
+        },
+]
+
+myCoding.forEach( (item) => {
+        console.log(item.languageName);
+})
